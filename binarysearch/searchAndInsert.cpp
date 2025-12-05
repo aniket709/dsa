e18c53a3183fcdeb8412a5d426f11f706b75e47c
aniket <aniket.ranjan709@gmail.com>
2025-12-05 13:07:56 +0530

    solve question using binary search

---
+++ binarysearch/searchAndInsert.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+using namespace std;
+
+int searchAndInsert(int arr[],int n,int k){
+
+    int start=0,end=n-1;
+
+    while (start<=end){
+        int mid = start+ (end-start)/2;
+        if (arr[mid]==k){
+            return mid; 
+        }
+        else if (arr[mid]>k){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    cout<<"inserting index is"<<" "<<start;
+    return start;
+}
+
+int main (){
+
+   int arr[5] = {2, 6, 7, 10,14};
+   searchAndInsert(arr,5,15);
+}
