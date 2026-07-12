d2eb954c141b91cd2b38150cb99a7ddc555010ae
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-07-12 17:04:40 +0530

    Time: 7 ms (18.93%), Space: 9.7 MB (76.23%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        int i=0;
+        int j=0;
+
+        while (i < s.length() || j< t.length() ){
+
+            if(s[i]!=t[j]){
+                return false;
+            }
+            else{
+                i++;
+                j++;
+            }
+        }
+        return true;
+    }
+};
