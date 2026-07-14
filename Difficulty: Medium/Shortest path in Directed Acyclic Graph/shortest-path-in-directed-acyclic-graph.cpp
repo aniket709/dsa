c7a797b05272695538c5857241737b131e8a9ee6
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-07-14 11:41:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,76 @@
+class Solution {
+  public:
+  
+   void dfs (int node, vector<pair<int,int>>adj[],int vis[],
+   stack<int> &st){
+       
+       vis[node]=1;
+       
+         for (auto it : adj[node]){
+            int v = it.first;
+            
+            if (!vis[v]){
+                dfs(v,adj,vis,st);
+            }
+        }
+        
+        st.push(node);
+   }
+   
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        vector<int> ans;
+        
+        int vis[V]={0};
+        
+        vector<pair<int,int>> adj[V];
+         
+          for (int i=0;i<E;i++){
+              
+              int u = edges[i][0];
+              int v= edges[i][1];
+              int wt = edges[i][2];
+              
+              adj[u].push_back({v,wt});
+          }
+          stack <int> st;
+        
+        for (int i=0;i<V;i++){
+            
+            if (!vis[i]){
+                
+                dfs (i,adj,vis,st);
+            }
+        }
+        
+        vector<int> dist(V,INT_MAX);
+        
+        dist[0]=0;
+        
+        while (!st.empty()){
+            
+            int node = st.top();
+            
+            st.pop();
+            
+             if (dist[node] != INT_MAX) {
+
+            for (auto it : adj[node]) {
+
+                int v = it.first;
+                int wt = it.second;
+
+                if (dist[node] + wt < dist[v]) {
+                    dist[v] = dist[node] + wt;
+                }
+            }
+        }
+    }
+        
+        for (int i = 0; i < V; i++) {
+    if (dist[i] == INT_MAX)
+        dist[i] = -1;
+}
+        
+              return dist;
+    }
+};
