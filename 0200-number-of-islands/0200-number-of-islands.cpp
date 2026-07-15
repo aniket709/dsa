ca05d387cb24860702148ed2e9c622e5b7b33e05
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-07-15 11:51:45 +0530

    Time: 27 ms (56.48%), Space: 23.6 MB (26.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,50 @@
 class Solution {
-public: 
- void dfs (int row,int col , vector<vector<bool>>&visited,vector<vector<char>>& grid ){
+public:
+ void bfs (int row,int col , vector<vector<bool>> &visited ,vector<vector<char>>& grid){
+         int n = grid.size();
+        int  m = grid[0].size();
+       queue<pair<int,int>> q;
+          int delrow[] ={-1,1,0,0};
+          int delcol[]={0,0,-1,1};
 
-        visited[row][col]=true;
-        int n = grid.size();
-        int m= grid[0].size();
+          q.push({row,col});
 
-        int delRow[]={-1,1,0,0};
-        int delCol[]={0,0,-1,1};
-      
-        for (int i=0;i<4;i++){
+           while (!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+              q.pop();
+              for (int i=0;i<4;i++){
+                int nrow = delrow[i] + row;
+                int ncol = delcol[i] + col;
 
-        int newrow = delRow[i] + row;
-        int newcol = delCol[i] + col;
+                if (nrow >=0 && nrow < n && ncol >=0 && ncol< m && !visited[nrow][ncol] && grid[nrow][ncol]=='1'){
+                    visited[nrow][ncol]=true;
+                    q.push({nrow,ncol});
+                }
+              }
+             
+         }
 
-
-       if (newrow >= 0 && newrow < n &&
-    newcol >= 0 && newcol < m &&
-    !visited[newrow][newcol] &&
-    grid[newrow][newcol] == '1')
-    {
-          
-
-            dfs (newrow,newcol,visited,grid);
-        }
-      }  
  }
     int numIslands(vector<vector<char>>& grid) {
 
         int n = grid.size();
-        int m= grid[0].size();
-
-         vector<vector<bool>>visited(n,vector<bool>(m,0));
-         int cnt=0;
-
-         for (int i=0;i<n;i++){
+        int  m = grid[0].size();
+        int cnt=0;
+         vector<vector<bool>> visited(n,vector<bool>(m,0));
+      
+         
+         for (int i =0;i<n;i++){
             for (int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    dfs (i,j,visited,grid);
-                      cnt++;
+                if ( !visited[i][j] && grid[i][j]=='1'){
+                    visited[i][j]= true;
+                   cnt++;
+                   bfs (i,j,visited,grid);
                 }
             }
          }
+          
+
 return cnt;
         
     }
