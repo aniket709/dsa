c465ef513c61f0c0d1e39aae38f8effc5cbf6f60
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-07-29 14:36:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set kth bit/set-kth-bit.cpp
+++ Difficulty: Basic/Set kth bit/set-kth-bit.cpp
@@ -1,66 +1,10 @@
 class Solution {
   public:
-  
-  string binary(int n) {
-    string str = "";
-
-    while (n != 0) {
-        int digit = n % 2;
-
-        str.push_back('0' + digit);
-
-        n = n / 2;
-    }
-
-    reverse(str.begin(), str.end());
-
-    return str;
-}
-
-void setBit(string &str, int k) {
-  
-    int index = str.length() - 1 - k;
-
-    
-    if (index < 0) {
-        str = string(k - str.length() + 1, '0') + str;
-        index = 0;
-    }
-
-    str[index] = '1';
-}
-
-int decimal(string &str) {
-
-    int i = str.length() - 1;
-    int power = 0;
-    int sum = 0;
-
-    while (i >= 0) {
-
-        if (str[i] == '1') {
-            sum += pow(2, power);
-        }
-
-        power++;
-        i--;
-    }
-
-    return sum;
-}
-
-int setKthBit(int n, int k) {
-
-   
-    string str = binary(n);
-
-
-    setBit(str, k);
-
-    
-    int sum = decimal(str);
-
-    return sum;
-
+    int setKthBit(int n, int k) {
+        
+        int ans = 1 << k;
+        
+        return ans | n;
+        
     }
 };
