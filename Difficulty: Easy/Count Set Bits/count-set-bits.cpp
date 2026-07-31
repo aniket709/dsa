691af5f59cfee459057c39be5a452853889278af
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-07-31 10:48:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int setBits(int n) {
+        
+      int cnt=0;
+      while (n){
+          
+            n = n & (n-1);
+      cnt++;
+          
+      }
+      
+      return cnt;
+   
+   
+        
+    }
+};
