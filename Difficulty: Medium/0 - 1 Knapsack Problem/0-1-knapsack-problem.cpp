2625968983f42eb0b48b6ce1dc1e76723099d229
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-08-11 17:03:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+  
+    int knapsacksolve (int W, vector<int> &val, vector<int> &wt,
+    int n,vector<vector<int>>& dp){
+        
+        if (n==0 || W==0) return 0;
+        
+        if (dp[n][W] != -1)
+            return dp[n][W];
+        
+        if (wt[n-1]<=W){
+            
+           return dp[n][W]= max 
+           (val[n-1]+ knapsacksolve(W-wt[n-1],val,wt,n-1,dp)
+             ,
+             knapsacksolve(W,val,wt,n-1,dp)
+            );
+        }
+            else
+            return dp[n][W]=  knapsacksolve(W,val,wt,n-1,dp);
+    
+    }
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+    
+      int n= wt.size();
+        vector<vector<int>> dp(n + 1,
+                               vector<int>(W + 1, -1));
+    return knapsacksolve (W,val,wt,n,dp);
+        
+    }
+};
