41df397b91dda2ada9e9dd92d772a7bbe89f61ca
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-08-12 14:21:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.cpp
+++ Difficulty: Medium/Count Subsets with Sum/count-subsets-with-sum.cpp
@@ -1,32 +1,56 @@
 class Solution {
   public:
-  int solve (vector<int>& arr, int target,int n,
-   vector<vector<int>> &dp){
+//   int solve (vector<int>& arr, int target,int n,
+//   vector<vector<int>> &dp){
       
-      if (target==0 && n==0) return 1;
-      if (target!=0 && n==0) return 0;
+//       if (target==0 && n==0) return 1;
+//       if (target!=0 && n==0) return 0;
       
-      if (dp[n][target]!=-1) return dp[n][target];
+//       if (dp[n][target]!=-1) return dp[n][target];
       
-      if (target>=arr[n-1]){
+//       if (target>=arr[n-1]){
           
-        dp[n][target]=  solve (arr,target-arr[n-1],n-1,dp) + 
-        solve (arr,target,n-1,dp);
-      }
-      else
+//         dp[n][target]=  solve (arr,target-arr[n-1],n-1,dp) + 
+//         solve (arr,target,n-1,dp);
+//       }
+//       else
       
-      dp[n][target]= solve (arr,target,n-1,dp);
+//       dp[n][target]= solve (arr,target,n-1,dp);
       
-      return dp[n][target];
+//       return dp[n][target];
       
-  }
+//   }
       
     int perfectSum(vector<int>& arr, int target) {
        
        int n = arr.size();
        
-       vector<vector<int>> dp (n+1,vector<int>(target+1,-1));
-     return   solve (arr,target,n,dp);
+    int dp [n+1][target+1];
+       
+       for (int i=0;i<n+1;i++){
+           for (int j=0;j<target+1;j++){
+               if(i==0) dp[i][j]=0;
+               
+               if (j==0) dp[i][j]=1;
+           }
+       }
+       
+       for (int i=1;i<n+1;i++){
+           for (int j=0;j<target+1;j++){
+               
+               if (arr[i-1]<=j){
+                   
+             dp[i][j] = dp[i-1][j-arr[i-1]] + dp [i-1][j];
+               }
+               else{
+                   
+                 dp[i][j]= dp[i-1][j];
+               }
+           }
+       }
+       
+       
+     return   dp[n][target];
         
     }
 };
