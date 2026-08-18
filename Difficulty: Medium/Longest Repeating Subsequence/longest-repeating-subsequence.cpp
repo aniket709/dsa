7b5b2093c9d0c331cd084ecdd46e35a278d78e5d
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-08-18 16:40:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    int longestRepSubseq(string &s) {
+     
+      string str= s;
+      int n= s.length();
+      int m= str.length();
+      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      
+      for (int i=0;i<n+1;i++){
+          for (int j=0;j<m+1;j++){
+              if (i==0) dp[i][j]=0;
+          }
+      }
+      
+      for (int i=1;i<n+1;i++){
+          for (int j=1;j<m+1;j++){
+              
+              if (s[i-1]==str[j-1] && i!=j){
+                  
+                  dp[i][j] =  1+ dp[i-1][j-1];
+              }
+              else{
+                  dp[i][j] = max (
+                      dp[i-1][j],
+                      dp[i][j-1]
+                      
+                      );
+              }
+              
+          }
+      }
+      
+      return dp[n][m];
+     
+        
+    }
+};
