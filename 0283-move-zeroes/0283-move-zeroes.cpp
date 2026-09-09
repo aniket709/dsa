012ca5dffe074f7febf4b42260d414251e1b01a6
aniket709 <142618124+aniket709@users.noreply.github.com>
2026-09-09 14:10:48 +0530

    Time: 3 ms (14.55%), Space: 24 MB (19.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
-        int i=0,j=0;
+        int i=0,j=0,n=nums.size();
 
-        while (j < nums.size()){
+        while (j < n){
 
             if (nums[i]==0 && nums[j]!=0){
-
-                swap (nums[i],nums[j]);
+                swap(nums[i],nums[j]);
                 i++;
                 j++;
             }
-             else if (nums[j]==0 && nums[i]!=0){
-              i++;
-             }
+            else if (nums[j]==0 && nums[i]!=0){
+            i++;
+            }
             else{
-
-                j++;
+               j++;
             }
         }
         
